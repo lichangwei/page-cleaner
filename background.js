faf4f4df0a40580301d99f331b5046dcffbd7dbc
lichangwei <lichangwei@love.com>
2012-05-30 18:05:19 +0800

    update

--- background.js
+++ background.js
@@ -1,28 +1,43 @@
+
+var ToolbarUIItemProperties = {
+    title : "Page Cleaner",
+    icon : "icons/hello-button.png",
+    onclick : function() {
+        if( button.disabled ) return;
+        var tab = opera.extension.tabs.getFocused();
+        if( tab ) tab.postMessage({'cmd': 'clean'});
+        button.disabled = true;
+    }
+};
+var button = opera.contexts.toolbar.createItem(ToolbarUIItemProperties);
+opera.contexts.toolbar.addItem(button);
+
 window.addEventListener("load", function() {
-    var ToolbarUIItemProperties = {
-        title : "Page Cleaner",
-        icon : "icons/hello-button.png",
-        onclick : function() {
-            
-        }
-    };
-    var button = opera.contexts.toolbar.createItem(ToolbarUIItemProperties);
-    opera.contexts.toolbar.addItem(button);
+    var template = document.getElementById('template').innerHTML;
     
-    opera.extension.onmessage = function(event){
-        window.opera.postError('Received message from base.js');
-        if(event.data === 'load'){
+    opera.extension.onconnect = function(e){
+        if(e && e.origin && e.origin.indexOf("popup.html") > -1 
+            && e.origin.indexOf('widget://') > -1){
+        }else{
+            event.source.postMessage( {'cmd': 'template', 'template': template} );
             enableButton();
         }
     }
-    
-    function enableButton(){
-        if( opera.extension.tabs.getFocused() ){
-            button.disabled = false;
-        }else{
+    opera.extension.onmessage = function(event){
+        var message = event.data;
+        if( message.cmd === 'disable-button' ){
             button.disabled = true;
+        }else if( message.cmd === 'enable-button' ){
+            button.disabled = false;
         }
     }
+    
     opera.extension.tabs.onfocus = enableButton;
     opera.extension.tabs.onblur = enableButton;
 }, false);
+
+function enableButton(){
+    var tab = opera.extension.tabs.getFocused();
+    if( tab ) button.disabled = false;
+    else button.disabled = true;
+}
--- includes/cleaner.js
+++ includes/cleaner.js
@@ -1,4 +1,49 @@
 
 window.addEventListener('load', function(){
-    opera.extension.postMessage('load');
+    
+    var background;
+    var template;
+    opera.extension.onmessage = function(event){
+        background = event.source;
+        var msg = event.data;
+        if( msg.cmd === 'clean' ){
+            clean();
+        }else if(msg.cmd === 'template'){
+            template = msg.template.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
+        }
+    };
+    
+    function clean(){
+        if( document.getElementById('oex-page-cleaner-template') ) return;
+        var div = document.createElement('div');
+        div.innerHTML = template;
+        var dom = div.firstChild;
+        var content = dom.getElementsByClassName('content')[0];
+        var fontSize = dom.getElementsByClassName('font-size')[0];
+        var close = dom.getElementsByClassName('close')[0];
+        
+        dom.style.height = window.innerHeight + 'px';
+        
+        fontSize.addEventListener('change', function(){
+            content.style.fontSize = fontSize.value;
+        }, false);
+        close.addEventListener('click', function(){
+            document.body.removeChild(dom);
+        }, false);
+        document.body.appendChild( dom );
+        content.innerHTML = document.title;
+    }
+    
+    function clean(){
+        var script = document.getElementById('oex-printfriendly');
+        if( script ) return;
+        script = document.createElement('script');
+        script.src = 'http://cdn.printfriendly.com/printfriendly.js';
+        script.type = 'text/javascript';
+        script.onload = function(){
+            window.print();
+        }
+        document.body.appendChild( script );
+    }
+
 }, false);
